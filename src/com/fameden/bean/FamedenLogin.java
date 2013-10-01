b8d991c1f00b899f4fca1a2671f02479863a5b1b
ravjot28@gmail.com <ravjot28@gmail.com>
2013-10-02 02:58:29 +0530

    asd

--- src/com/fameden/bean/FamedenLogin.java
+++ src/com/fameden/bean/FamedenLogin.java
@@ -3,6 +3,16 @@ package com.fameden.bean;
 public class FamedenLogin {
 
 	private String userID ;
+	
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getUserID() {
 		return userID;
