b3070e3e4b9bf30aaeb6900155da965954e83b58
ravjot28 <ravjot28@gmail.com>
2013-10-13 16:24:43 +0530

    RSA local commit

--- src/com/fameden/util/RSAEncryptionKeyGeneration.java
+++ src/com/fameden/util/RSAEncryptionKeyGeneration.java
@@ -2,18 +2,19 @@ package com.fameden.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import javax.crypto.Cipher;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
  * @author JavaDigest
@@ -26,7 +27,7 @@ public class RSAEncryptionKeyGeneration {
 
 	public static final String PUBLIC_KEY_FILE = "public.key";
 
-	public static void generateKey() {
+	private static void generateKey() {
 		try {
 			final KeyPairGenerator keyGen = KeyPairGenerator
 					.getInstance(ALGORITHM);
@@ -61,7 +62,7 @@ public class RSAEncryptionKeyGeneration {
 
 	}
 
-	public static boolean areKeysPresent() {
+	private static boolean areKeysPresent() {
 
 		File privateKey = new File(PRIVATE_KEY_FILE);
 		File publicKey = new File(PUBLIC_KEY_FILE);
@@ -72,52 +73,85 @@ public class RSAEncryptionKeyGeneration {
 		return false;
 	}
 
-	public static byte[] encrypt(String text, PublicKey key) {
+	private static byte[] encrypt(String text) {
+
+		ObjectInputStream inputStream = null;
 		byte[] cipherText = null;
 		try {
+			inputStream = new ObjectInputStream(new FileInputStream(
+					PUBLIC_KEY_FILE));
+			PublicKey key = (PublicKey) inputStream.readObject();
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.ENCRYPT_MODE, key);
 			cipherText = cipher.doFinal(text.getBytes());
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		return cipherText;
 	}
 
-	public static String decrypt(byte[] text, PrivateKey key) {
+	public static String encryptText(String text) {
+		return new BASE64Encoder().encode(encrypt(text));
+	}
+
+	private static String decrypt(byte[] text) {
+
+		ObjectInputStream inputStream = null;
 		byte[] dectyptedText = null;
 		try {
+			inputStream = new ObjectInputStream(new FileInputStream(
+					PRIVATE_KEY_FILE));
+			PrivateKey key = (PrivateKey) inputStream.readObject();
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.DECRYPT_MODE, key);
 			dectyptedText = cipher.doFinal(text);
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 		return new String(dectyptedText);
 	}
 
-	public static void main(String[] args) {
+	public static String decryptText(String input) {
+		String result = null;
+		try {
+			result = decrypt(new BASE64Decoder().decodeBuffer(input));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		return result;
+	}
+
+	public static void main(String[] args) {
 		try {
 			if (!areKeysPresent()) {
 				generateKey();
 			}
 
-			final String originalText = "Text to be encrypted ";
-			ObjectInputStream inputStream = null;
-			inputStream = new ObjectInputStream(new FileInputStream(
-					PUBLIC_KEY_FILE));
-			final PublicKey publicKey = (PublicKey) inputStream.readObject();
-			final byte[] cipherText = encrypt(originalText, publicKey);
-			inputStream = new ObjectInputStream(new FileInputStream(
-					PRIVATE_KEY_FILE));
-			final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
-			final String plainText = decrypt(cipherText, privateKey);
-			System.out.println("Original Text: " + originalText);
-			System.out.println("Encrypted Text: " + cipherText.toString());
-			System.out.println("Decrypted Text: " + plainText);
+			final String originalText = "ravjot28   ";
+
+			String source = encryptText(originalText);
+
+			String dest = decryptText(source);
+
+			System.out.println(source + "\n" + dest);
+			
+			System.out.println(originalText.length()+" "+source.length() + " " + dest.length());
 
 		} catch (Exception e) {
 			e.printStackTrace();
